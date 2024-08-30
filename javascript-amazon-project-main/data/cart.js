bb1ca8575825f1fc76a49478bcff585c8dff6886
Troyboateng <dominic.asiedu1@stu.ucc.edu.gh>
2024-08-30 12:31:18 -0230

    Delete products from Cart when clicking delete

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,5 @@
-export const cart = [{
+
+export let cart = [{
     productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2
 }, {
@@ -6,6 +7,7 @@ export const cart = [{
     quantity: 1
 }];
 
+// Add to cart function
 
 export function addToCart (productId) {
 
@@ -29,3 +31,17 @@ export function addToCart (productId) {
                       }
   }
   
+  // Remove Cart Function
+
+  export function removeFromCart(productId){
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart
+
+  }
